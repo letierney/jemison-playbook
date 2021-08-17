e1a8804e6f93b75936d5f5898ea4c26ad307a5b7
jtattum <74188198+jtattum@users.noreply.github.com>
2021-08-17 17:39:17 +0100

    Added AND favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   organizationName: 'jtattum94', // Usually your GitHub org/user name.
   projectName: 'jemison-playbook', // Usually your repo name.
   themeConfig: {
